f0540ee82e135f8ff56ed1d8af36cda0296c3e1b
ZiyadZK <kakangtea74@gmail.com>
2024-04-08 11:46:29 +0700

    Rework and Relayout Login Page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,9 @@
 'use client'
 
 
+import { mont, rale } from "@/config/fonts";
 import { loginAkun } from "@/lib/model/akunModel";
-import { faEnvelope, faExclamationCircle, faKey, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faExclamationCircle, faExclamationTriangle, faKey, faSignIn, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Nunito, Quicksand } from "next/font/google";
 import Image from "next/image";
@@ -10,8 +11,11 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
-const nunito = Nunito({subsets: ['latin']})
-const quicksand = Quicksand({subsets: ['latin']})
+let imageLinks = [
+    'https://wallpapers.com/images/featured/vector-art-6ttd2h971c0ivqyh.jpg',
+    'https://wallpapercave.com/wp/wp4506069.jpg',
+    'https://free4kwallpapers.com/uploads/originals/2020/11/15/sunset-vector-wallpaper.jpg'
+]
 
 export default function LoginPage() {
     const router = useRouter();
@@ -46,105 +50,57 @@ export default function LoginPage() {
     }
 
     return (
-        <div className={`${quicksand.className} grid grid-cols-2 bg-white w-full h-screen`}>
+        <div className="relative overflow-hidden w-full h-screen">
             <Toaster />
-            <div className="w-full h-full relative overflow-hidden group hidden md:block ">
-                <img className="w-full h-full object-center object-cover" src="https://majalahsora.com/wp-content/uploads/2016/06/smk-pu.jpg" alt="" />
-                <div className="absolute top-0 left-0 w-full h-full flex items-center p-5 opacity-0 group-hover:opacity-100 transition-all duration-300 bg-black/80 backdrop-blur-md">
-                    <div className="w-full">
-                        <div className="flex w-full justify-center items-center gap-5">
-                            <Image src={'/logo-sekolah.png'} width={40} height={40} alt="Logo sekolah" />
-                            <h1 className={`${nunito.className} text-3xl font-bold text-white`}>
-                                Sistem Manajemen <span className="text-orange-600">Akademik</span> <br />
-                                <span className="font-light text-lg text-white">
-                                    SMK Pekerjaan Umum Negeri Bandung
-                                </span>
-                            </h1>
+            <img src={`${imageLinks[0]}`} className="w-full h-full object-cover" alt="" />
+            <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center backdrop-blur">
+                <div className="w-full md:w-1/3 p-5 md:p-8 md:rounded-2xl bg-white h-full md:h-fit flex flex-col md:block justify-center">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-2 p-2 md:p-3 ">
+                            <img className="w-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png" alt="" />
+                            <h1 className={`${rale.className} text-zinc-600 font-bold`}>SIM<span className="text-orange-600">AK</span></h1>
                         </div>
-                        <div className="w-full flex justify-center mt-5 text-white">
-                            <p className="font-extralight text-center">
-                                Sistem yang berfungsi untuk mengelola data-data <br />
-                                yang ada disekolah secara Efisien dan Efektif.
-                            </p>
-                        </div>
+                        <h1 className={`${rale.className} text-zinc-700 text-xs md:text-md text-end`}>Sistem Manajemen <br /> Akademik SMK PUN Bandung</h1>
                     </div>
-                </div>
-            </div>
-            <div className="w-full p-5 col-span-2 md:col-span-1">
-                <div className="flex items-center gap-5 w-full">
-                    <Image src={'/logo-sekolah.png'} width={25} height={25} alt="Logo sekolah" />
-                    <article>
-                        <h1 className="text-zinc-800 font-extrabold">
-                            Siska<span className="text-orange-600">pun</span>
-                        </h1>
-                        <p className="text-sm text-zinc-800 font-extralight">
-                            Sistem Manajemen Akademik
-                        </p>
-                    </article>
-                </div>
-                <div className="my-10">
-                    <div className="flex w-full justify-center text-zinc-800">
-                        <h1 className="text-xl md:text-4xl">
-                            Hai, <b>Selamat Datang!</b>
-                        </h1>
-                    </div>
-                </div>
-                <div className="flex w-full justify-center text-zinc-800">
-                    <p className="text-center text-sm md:text-lg">
-                        Gunakan akun anda untuk masuk ke dalam <b>Dashboard</b>
-                    </p>
-                </div>
-                <form onSubmit={submitLogin} className="flex justify-center w-full text-zinc-800 my-5">
-                    <div className="md:w-2/3 w-full space-y-3">
-                        <div className="w-full relative">
-                            <input required value={loginForm.email} onChange={e => setLoginForm(state => state = {...state, email: e.target.value})} type="email" name="email" className="rounded px-4 py-2 w-full border border-zinc-600 placeholder-shown:border-zinc-400 outline-none hover:border-zinc-600 focus:border-zinc-600 bg-zinc-100 peer transition-all duration-300" autoComplete="off" placeholder="Email anda disini"/>
-                            <div className="absolute top-0 right-0 w-10 h-full flex items-center justify-center z-50 peer-placeholder-shown:text-zinc-400 text-zinc-700 transition-all duration-300">
-                                <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5 text-inherit" />
+                    <hr className="my-5 md:my-2 opacity-0" />
+                    <h1 className={`${rale.className} text-zinc-800 font-medium text-2xl`}>Selamat Datang,</h1>
+                    <p className={`${rale.className} text-zinc-600 text-xs md:text-sm`}>Silahkan gunakan akun anda untuk masuk ke Dashboard SIMAK</p>
+                    <hr className="my-5 md:my-2 opacity-0" />
+                    <form onSubmit={submitLogin}>
+                        <div className="relative h-10">
+                            <div className="absolute top-0 left-0 w-10 h-10 flex items-center justify-center">
+                                <FontAwesomeIcon icon={faEnvelope} className="w-4 h-4 text-zinc-500" />
                             </div>
+                            <input required type="text" onChange={e => setLoginForm(state => ({...state, email: e.target.value}))} className="w-full h-full rounded-lg border outline outline-offset-2 outline-2 outline-zinc-300/0 focus:outline-zinc-300/100 bg-transparent pl-10 text-zinc-800" placeholder="Email / Username anda" />
                         </div>
-                        <hr className="w-full opacity-0" />
-                        <div className="w-full relative">
-                            <input required value={loginForm.password} onChange={e => setLoginForm(state => state = {...state, password: e.target.value})} type={showPass ? 'text' : 'password'} name="password" className="rounded px-4 py-2 w-full border border-zinc-600 placeholder-shown:border-zinc-400 outline-none hover:border-zinc-600 focus:border-zinc-600 bg-zinc-100 peer transition-all duration-300" autoComplete="off" placeholder="Password anda disini"/>
-                            <div className="absolute top-0 right-0 w-10 h-full flex items-center justify-center z-50 peer-placeholder-shown:text-zinc-400 text-zinc-700 transition-all duration-300">
-                                <FontAwesomeIcon icon={faKey} className="w-5 h-5 text-inherit" />
+                        <hr className="my-2 opacity-0" />
+                        <div className="relative h-10">
+                            <div className="absolute top-0 left-0 w-10 h-10 flex items-center justify-center">
+                                <FontAwesomeIcon icon={faKey} className="w-4 h-4 text-zinc-500" />
                             </div>
+                            <input required type={`${showPass ? 'text' : 'password'}`} onChange={e => setLoginForm(state => ({...state, password: e.target.value}))} className="w-full h-full rounded-lg border outline outline-offset-2 outline-2 outline-zinc-300/0 focus:outline-zinc-300/100 bg-transparent pl-10 text-zinc-800" placeholder="Password anda" />
                         </div>
-                        <div className="flex w-full justify-between items-center text-zinc-800">
-                            <div className="flex items-center gap-3">
-                                <input type="checkbox" name="logged" id="logged" className="accent-black outline-none" />
-                                <label htmlFor="logged" className="text-sm">Ingat <b>Login</b></label>
+                        <hr className="my-1 opacity-0" />
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" id="rememberme" defaultChecked className="checkbox checkbox-sm checkbox-warning" />
+                                <label htmlFor="rememberme" className={`${rale.className} text-zinc-500 text-sm`}>Ingat Login</label>
                             </div>
-                            <button onClick={() => setShowPass(state => !state)} type="button" className="text-sm font-bold hover:text-orange-600">
-                                {showPass ? 'Sembunyikan Password' : 'Tunjukan Password'}
+                            <button type="button" onClick={() => setShowPass(state => !state)} className={`${rale.className} text-zinc-500 text-xs md:text-sm hover:text-blue-600`}>
+                                {showPass ? 'Sembunyikan' : 'Lihat'} Password
                             </button>
                         </div>
-                        <hr className="my-3 opacity-0" />
-                        <div className="flex w-full justify-center">
-                            {!loginLoading ? <button  type="submit" className="w-1/2 border py-2 rounded bg-zinc-800 text-white hover:bg-zinc-900 focus:bg-zinc-900">
-                                Masuk
-                            </button> : <button disabled type="submit" className="w-1/2 border py-2 rounded bg-zinc-800 text-white hover:bg-zinc-900 focus:bg-zinc-900">
-                                <FontAwesomeIcon icon={faSpinner} className="animate-spin w-5 h-5 text-white" />
-                            </button>}
-                        </div>
-                        <hr className="my-3 opacity-0" />
-                        <div className="grid grid-cols-2">
-                            <a href="" className="px-3 tracking-tighter font-medium hover:text-orange-600 my-2 w-full flex justify-center text-center">
-                                Lupa Password
-                            </a>
-                            <a href="" className="px-3 tracking-tighter font-medium hover:text-orange-600 my-2 w-full flex justify-center text-center">
-                                Masuk sebagai Siswa
-                            </a>
-                        </div>
-                    </div>
-                </form>
-                <div className="text-zinc-800 grid grid-cols-12 gap-5 md:hidden">
-                    <div className="w-full flex justify-center col-span-2">
-                        <FontAwesomeIcon icon={faExclamationCircle} className="w-5 h-5 text-red-500" />
-                    </div>
-                    <div className="w-full col-span-10 text-sm">
-                        <p className="font-bold">Perhatian!</p>
-                        <p>Dianjurkan untuk menggunakan <b>Laptop</b> atau <b>PC</b> untuk menghasilkan tampilan yang maksimal!</p>
-                    </div>
+                        <hr className="my-2 opacity-0" />
+                        <button type="submit" {...loginLoading && 'disabled'} className={`${rale.className} btn w-full text-white flex items-center justify-center gap-5`}>
+                            <FontAwesomeIcon icon={loginLoading ? faSpinner : faSignIn} className={`w-4 h-4 text-inherit ${loginLoading && 'animate-spin'}`} />
+                            {!loginLoading && 'Masuk'}
+                        </button>
+                    </form>
+                    <hr className="w-full my-3" />
+                    <a href="#" className={`${rale.className} btn w-full bg-transparent border-zinc-300 flex items-center justify-center text-zinc-600 font-medium gap-2 hover:bg-zinc-100`}>
+                        <FontAwesomeIcon icon={faExclamationTriangle} className="w-4 h-4 text-red-700" />
+                        Lupa Password?
+                    </a>
                 </div>
             </div>
         </div>
