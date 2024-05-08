cd366aa29b9291d193bd19a141c3d46e0a5bcd34
Ziyad <kakangtea74@gmail.com>
2024-05-08 15:41:49 +0700

    Working on placing charts / stats on Main layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,46 @@
+'use client'
+
 import MainLayoutPage from "@/components/mainLayout";
-import { quicksand } from "@/config/fonts";
-import Image from "next/image";
+import { mont } from "@/config/fonts";
+import { faList, faPieChart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
+
+  const [display, setDisplay] = useState('simple')
+
   return (
     <MainLayoutPage>
-      <div className="p-5">
-        This is Main Page
+      <Toaster />
+      <div className="mt-5">
+        <div className="flex items-center gap-5 w-full md:w-fit">
+          <button type="button" onClick={() => setDisplay('simple')} disabled={display === 'simple'} className={`px-4 py-2 w-full md:w-fit rounded-full ${display === 'simple' ? 'bg-zinc-200' : 'bg-zinc-50 hover:bg-zinc-100 focus:bg-zinc-200'} text-zinc-700 flex items-center justify-center gap-3 text-xl md:text-2xl`}>
+            <FontAwesomeIcon icon={faList} className="w-4 h-4 text-inherit" />
+            Simple
+          </button>
+          <button type="button" onClick={() => setDisplay('chart')} disabled={display === 'chart'} className={`px-4 py-2 w-full md:w-fit rounded-full ${display === 'chart' ? 'bg-zinc-200' : 'bg-zinc-50 hover:bg-zinc-100 focus:bg-zinc-200'} text-zinc-700 flex items-center justify-center gap-3 text-xl md:text-2xl`}>
+            <FontAwesomeIcon icon={faPieChart} className="w-4 h-4 text-inherit" />
+            Chart
+          </button>
+        </div>
+        <hr className="my-3 opacity-0" />
+        {display === 'simple' && (
+          <div className="">This is Display Simple</div>
+        )}
+        {display === 'chart' && (
+          <div className={`${mont.className}`}>
+            <div className="flex flex-col md:flex-row gap-5">
+              <div className="p-5 rounded-lg border w-full md:w-1/3">
+                <div className="flex items-center">
+                  <div className="radial-progress text-primary" style={{"--value":70}} role="progressbar">70%</div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        )}
       </div>
     </MainLayoutPage>
   );
--- src/components/mainLayout.jsx
+++ src/components/mainLayout.jsx
@@ -4,7 +4,7 @@ import { mont, open, quicksand, rale } from "@/config/fonts";
 // import { nunito, quicksand } from "@/config/fonts";
 import { getLoggedUserdata, logoutAkun } from "@/lib/model/akunModel";
 import { navigator } from "@/lib/navigator";
-import { faBars, faBook, faCertificate, faClipboard, faCog, faCogs, faFolderTree, faHouse, faLayerGroup, faSignOut, faSpinner, faUserGraduate, faUserLock, faUserShield, faUserTie, faUserXmark, faUsersBetweenLines, faUsersRectangle, faUsersViewfinder, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faBook, faCertificate, faClipboard, faCog, faCogs, faDatabase, faFolderTree, faHouse, faLayerGroup, faQuidditchBroomBall, faSignOut, faSpinner, faTimeline, faUserGraduate, faUserLock, faUserShield, faUserTie, faUserXmark, faUsersBetweenLines, faUsersRectangle, faUsersViewfinder, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Nunito, Quicksand } from "next/font/google";
 import Image from "next/image";
@@ -23,7 +23,8 @@ const navLinkMasterData = [
     { title: 'Sertifikasi', icon: faCertificate, url: '/data/sertifikasi', page: 'Data Sertifikasi'},
     { title: 'Ijazah', icon: faCertificate, url: '/data/ijazah', page: 'Data Ijazah'},
     { title: 'Kelas', icon: faUsersRectangle, url: '/data/kelas', page: 'Data Kelas'},
-    { title: 'Akun', icon: faUserShield, url: '/data/akun', page: 'Data Akun'}
+    { title: 'Akun', icon: faUserShield, url: '/data/akun', page: 'Data Akun'},
+    { title: 'Riwayat', icon: faTimeline, url: '/data/riwayat', page: 'Data Riwayat Perubahan Data'}
 ]
 
 const navLinkKonfigurasi = [
@@ -177,14 +178,7 @@ export default function MainLayoutPage({children}) {
                                 </a>
                             ))}
                         </div>
-                        <div className="flex items-center gap-3 my-5 text-xs font-medium">
-                            <hr className="flex-grow" />
-                            <div className="flex items-center gap-3 w-fit">
-                                <FontAwesomeIcon icon={faCogs} className="w-3 h-3 text-inherit" />
-                                Tentang
-                            </div>
-                            <hr className="flex-grow" />
-                        </div>
+                        
                     </div>
                 </div>
                 <div className={`${rale.className} w-full md:w-10/12 px-5 pt-16 h-full text-zinc-800 relative overflow-auto`}>
