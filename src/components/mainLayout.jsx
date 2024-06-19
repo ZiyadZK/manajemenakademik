7c12e210d4692ece9efb3cd1d2a579839faa9ac1
Ziyad <kakangtea74@gmail.com>
2024-06-19 11:24:47 +0700

    adding new layout

--- src/components/mainLayout.jsx
+++ src/components/mainLayout.jsx
@@ -5,6 +5,9 @@ import { mont, rale, open, quicksand, jakarta } from "@/config/fonts";
 import { getLoggedUserdata, logoutAkun } from "@/lib/model/akunModel";
 import { faBars, faBook, faCertificate, faClipboard, faCog, faCogs, faDatabase, faFolderTree, faHouse, faLayerGroup, faMoon, faPrint, faQuidditchBroomBall, faSignOut, faSpinner, faSun, faTimeline, faUserGraduate, faUserLock, faUserShield, faUserTie, faUserXmark, faUsersBetweenLines, faUsersRectangle, faUsersViewfinder, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
@@ -84,7 +87,7 @@ export default function MainLayoutPage({children}) {
         getTheme()
     }, [])
 
-    const [showSidebar, setShowSidebar] = useState(false)
+    const [hoveredPath, setHoveredPath] = useState(path)
 
     const submitLogout = async () => {
         return mySwal.fire({
@@ -110,101 +113,136 @@ export default function MainLayoutPage({children}) {
     }
 
     return (
-        <div className="w-full h-screen bg-white dark:bg-zinc-900">
-            {showSidebar && <SidebarSection setShowSidebar={setShowSidebar} />}
-            <nav className="fixed top-0 left-0 px-5 py-3 border-b border-zinc-300 dark:border-zinc-800 shadow-lg w-full flex items-center z-50 bg-white dark:bg-zinc-900">
-                <div className="w-2/12">    
-                    <div className="flex items-center gap-5">
-                        <img className="w-5 hidden md:block" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png" alt="" />
-                        <h1 className={`${rale.className} font-bold text-gray-800 dark:text-zinc-200 text-xs md:text-sm`}>SIM<span className="text-orange-600">AK.</span></h1>
-                    </div>
-                </div>
-                <div className="w-10/12 flex justify-between items-center">
-                    <div className="flex items-center gap-3">   
-                        <button type="button" onClick={() => setShowSidebar(state => !state)} className="md:hidden flex items-center justify-center swap swap-rotate  text-blue-600 btn btn-sm bg-transparent border-0">
-                            <FontAwesomeIcon icon={showSidebar ? faXmark : faBars} className="w-3 h-3 text-inherit" />
-                        </button>
-                        <h1 className={`${rale.className} text-blue-800 dark:text-blue-500 hidden md:flex font-medium text-xl tracking-wide w-fit  items-center gap-3`}>
-                            <FontAwesomeIcon icon={filteredPath ? filteredPath.icon : faSpinner} className={`w-4 h-4 text-inherit ${!filteredPath ? 'animate-spin' : ''}`} />
-                            {filteredPath ? filteredPath.page : ''}
-                        </h1>
-                    </div>
-                    <div className="flex items-center gap-3  rounded px-3 py-1">
-                        <article className="text-end">
-                            <h1 className={`${rale.className} text-zinc-700 dark:text-zinc-400 text-xs font-medium md:text-sm`}>
-                                {loggedAkun !== null ? loggedAkun.email_akun : <div className="w-16 h-4 bg-zinc-300 animate-pulse rounded"></div>}
-                            </h1>
-                            <p className={`${rale.className} text-zinc-700 text-xs dark:text-zinc-500`}>
-                                {loggedAkun !== null ? loggedAkun.role_akun : <span className="w-16 h-4 bg-zinc-300 animate-pulse rounded"></span>}
-                            </p>
-                        </article>
-                        <div className="flex items-center gap-2">
-                            <button type="button" onClick={() => submitLogout()} className={`${rale.className} btn btn-error btn-sm flex items-center justify-center gap-3`}>
-                                <FontAwesomeIcon icon={faSignOut} className="w-3 h-3 text-inherit" />
-                                <span className="hidden md:block">Keluar</span>
-                            </button>
+        <div className={`drawer ${jakarta.className} dark:text-zinc-100 text-zinc-700`}>
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content w-full bg-zinc-100 dark:bg-zinc-950">
+                <div className="px-5 bg-white sticky top-0 dark:bg-zinc-900 border-b border-zinc-300 dark:border-zinc-800 pt-5 space-y-2 z-10">
+                    <div className="flex justify-between w-full items-center">
+                        <div className="flex items-center divide-x divide-zinc-300 dark:divide-zinc-800">
+                            <Link href={'/'} className="flex items-center gap-3 pr-3 md:pr-5">
+                                <Image src={'/logo-sekolah-2.png'} width={15} height={15} alt="Logo Sekolah" />
+                                <h1 className="font-bold tracking-tighter hidden md:block">
+                                    SIM<span className="text-orange-500">AK</span>
+                                </h1>
+                            </Link>
+                            {!filteredPath ? (
+                                <div className="loading loading-spinner loading-sm text-zinc-400 pl-3 md:pl-5 py-3"></div>
+                            ):(
+                                <div className="pl-3 md:pl-5 flex items-center gap-3">
+                                    <FontAwesomeIcon icon={filteredPath.icon} className="w-3 h-3 text-inherit" />
+                                    {filteredPath.title}
+                                </div>
+                            )}
                         </div>
-                    </div>
-                </div>
-            </nav>
-            <div className={`flex h-full ${mont.className}`}>
-                <div className="hidden md:block relative overflow-auto w-2/12 border-r border-zinc-300 dark:border-zinc-800 h-full text-zinc-800 pt-16 bg-zinc-50 dark:bg-zinc-950">
-                    <hr className="my-2 opacity-0" />
-                    <div className="px-5">
-                        <a href="/" className={`flex items-center justify-between tracking-tighter text-sm ${filteredPath && filteredPath.url === '/' ? 'bg-white dark:bg-zinc-800/50 shadow-lg dark:shadow-black/50 text-zinc-800 dark:text-white' : 'hover:bg-zinc-200 dark:hover:bg-zinc-800/50 text-zinc-400 dark:text-zinc-700 hover:text-zinc-600 dark:hover:text-zinc-500 group'}  font-medium rounded-lg px-3 py-2`}>
-                            <span className="flex items-center gap-5">
-                                <FontAwesomeIcon icon={faHouse} className={`w-4 h-4  ${filteredPath && filteredPath.url === '/' ? 'text-rose-600' : 'text-zinc-300 dark:text-zinc-600 group-hover:text-zinc-400'} `} />
-                                Dashboard
-                            </span>
-                            <span className={`w-2 h-2 rounded-full bg-blue-600/50 animate-ping ${filteredPath && filteredPath.url === '/' ? 'block' : 'hidden'} `}></span>
-                        </a>
-                        <a href="/profilsekolah" className={`flex items-center justify-between tracking-tighter text-sm ${filteredPath && filteredPath.url === '/profilsekolah' ? 'bg-white dark:bg-zinc-800/50 shadow-lg dark:shadow-black/50 text-zinc-800 dark:text-white' : 'hover:bg-zinc-200 dark:hover:bg-zinc-800/50 text-zinc-400 dark:text-zinc-700 hover:text-zinc-600 dark:hover:text-zinc-500 group'}  font-medium rounded-lg px-3 py-2`}>
-                            <span className="flex items-center gap-5">
-                                <FontAwesomeIcon icon={faBook} className={`w-4 h-4  ${filteredPath && filteredPath.url === '/profilsekolah' ? 'text-rose-600' : 'text-zinc-300 dark:text-zinc-600 group-hover:text-zinc-400'} `} />
-                                Profil Sekolah
-                            </span>
-                            <span className={`w-2 h-2 rounded-full bg-blue-600/50 ${filteredPath && filteredPath.url === '/profilsekolah' ? 'block' : 'hidden'} `}></span>
-                        </a>
-                        <div className="flex items-center gap-3 my-5 text-xs font-medium">
-                            <hr className="flex-grow dark:opacity-30" />
-                            <div className="flex items-center gap-3 w-fit dark:text-white">
-                                <FontAwesomeIcon icon={faLayerGroup} className="w-3 h-3 text-inherit dark:text-blue-600" />
-                                Master Data
+                        {!loggedAkun ? (
+                            <div className="loading loading-spinner loading-sm text-zinc-400 py-2"></div>
+                        ):(
+                            <div className="flex items-center gap-3">
+                                <div className="hidden md:flex items-center gap-3 ">
+                                    <p className="px-2 py-1 rounded-full text-xs font-medium bg-red-500/20 text-red-500">
+                                        Admin
+                                    </p>
+                                    <article className="text-end">
+                                        <p className="text-xs">
+                                            kakangtea74@gmail.com
+                                        </p>
+                                        <p className="text-xs opacity-50">
+                                            Ziyad
+                                        </p>
+                                    </article>
+                                </div>
+                                <button type="button" onClick={() => toggleTheme()} className="rounded-full border w-8 h-8 dark:border-zinc-800 hover:bg-zinc-50 dark:hover:bg-zinc-800 transition-all duration-300 hidden md:flex items-center justify-center group text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-200">
+                                    <FontAwesomeIcon icon={theme === 'light' ? faMoon : faSun} className="w-4 h-4 text-inherit group-hover:-rotate-45 transition-all duration-300" />
+                                </button>
+                                <label htmlFor="my-drawer" className="rounded-full border w-8 h-8 dark:border-zinc-800 hover:bg-zinc-50 transition-all duration-300 dark:hover:bg-zinc-800 flex md:hidden items-center justify-center group text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-200 drawer-button">
+                                    <FontAwesomeIcon icon={faCog} className="w-4 h-4 text-inherit group-hover:-rotate-45 transition-all duration-300" />
+                                </label>
+                                <button type="button" onClick={() => submitLogout()} className="rounded-full border w-8 h-8 dark:border-zinc-800 hover:bg-zinc-50 transition-all duration-300 dark:hover:bg-zinc-800 hidden md:flex items-center justify-center group text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-200">
+                                    <FontAwesomeIcon icon={faSignOut} className="w-4 h-4 text-inherit transition-all duration-300" />
+                                </button>
                             </div>
-                            <hr className="flex-grow dark:opacity-30" />
-                        </div>
-                        <div className="space-y-2">
+                        )}
+                    </div>
+                    {!filteredPath ? (
+                        <div className="loading loading-spinner loading-sm text-zinc-400 py-4"></div>
+                    ):(
+                        <div className="flex items-center gap-2 overflow-auto relative w-full pb-2">
+                            <Link
+                                href={'/profilsekolah'}
+                                className={`px-4 py-2 rounded-md text-xs font-medium ${filteredPath && filteredPath.url.startsWith('/profilsekolah') ? 'text-zinc-800 dark:text-zinc-100' : 'text-zinc-400'} hover:text-zinc-700 dark:hover:text-zinc-200 relative no-underline duration-300 ease-in z-[100]`}
+                                onMouseOver={() => setHoveredPath('/profilsekolah')}
+                                onMouseLeave={() => setHoveredPath(path)}
+                            >
+                                <span>
+                                    Profil Sekolah
+                                </span>
+                                {hoveredPath.startsWith('/profilsekolah') && (
+                                    <motion.div
+                                        className="absolute bottom-0 left-0 h-full bg-zinc-100 dark:bg-zinc-800 rounded-md -z-10 text-zinc-700 dark:text-zinc-200"
+                                        layoutId="navbar"
+                                        aria-hidden="true"
+                                        style={{
+                                            width: "100%",
+                                        }}
+                                        transition={{
+                                            type: 'spring',
+                                            bounce: 0,
+                                            stiffness: 200,
+                                            damping: 30,
+                                            duration: 0.01,
+                                        }}
+                                    />
+                                )}
+                                
+                            </Link>
                             {loggedAkun && navLinkMasterData.map((nav, index) => nav['role'].includes(loggedAkun.role_akun) && (
-                                <a key={index} href={nav.url} className={`flex items-center text-sm justify-between tracking-tighter ${filteredPath && filteredPath.url.startsWith(nav.url) ? 'bg-white dark:bg-zinc-800/50 shadow-lg text-zinc-800 dark:text-zinc-400' : 'hover:bg-zinc-200 dark:hover:bg-zinc-800/50 text-zinc-400 dark:text-zinc-600 hover:text-zinc-600 dark:hover:text-zinc-400 group'}  font-medium rounded-lg px-3 py-2`}>
-                                    <span className="flex items-center gap-6">
-                                        <FontAwesomeIcon icon={nav.icon} className={`w-4 h-4  ${filteredPath && filteredPath.url.startsWith(nav.url) ? 'text-blue-600' : 'text-zinc-300 dark:text-zinc-600 group-hover:text-zinc-400'} `} />
+                                <Link
+                                    key={index} 
+                                    href={nav.url}
+                                    className={`px-4 py-2 rounded-md text-xs font-medium ${filteredPath && filteredPath.url.startsWith(nav.url) ? 'text-zinc-800 dark:text-zinc-100' : 'text-zinc-400'} hover:text-zinc-700 dark:hover:text-zinc-200 relative no-underline duration-300 ease-in z-[100]`}
+                                    onMouseOver={() => setHoveredPath(nav.url)}
+                                    onMouseLeave={() => setHoveredPath(path)}
+                                >
+                                    <span>
                                         {nav.title}
                                     </span>
-                                    <span className={`w-2 h-2 rounded-full bg-blue-600/50 ${filteredPath && filteredPath.url.startsWith(nav.url) ? 'block' : 'hidden'} animate-ping`}></span>
-                                </a>
+                                    {hoveredPath.startsWith(nav.url) && (
+                                        <motion.div
+                                            className="absolute bottom-0 left-0 h-full bg-zinc-100 dark:bg-zinc-800 rounded-md -z-10 text-zinc-700 dark:text-zinc-200"
+                                            layoutId="navbar"
+                                            aria-hidden="true"
+                                            style={{
+                                                width: "100%",
+                                            }}
+                                            transition={{
+                                                type: 'spring',
+                                                bounce: 0,
+                                                stiffness: 200,
+                                                damping: 30,
+                                                duration: 0.01,
+                                            }}
+                                        />
+                                    )}
+                                    
+                                </Link>
                             ))}
                         </div>
-                        <hr className="my-5 dark:opacity-30" />
-                        <div className="flex justify-between items-center">
-                            <p className="text-sm opacity-60 dark:opacity-100 dark:text-zinc-600">
-                                Ubah Tema
-                            </p>
-                            <button type="button" onClick={() => toggleTheme()} className="flex items-center gap-2 p-2 rounded-lg bg-zinc-100 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400 text-sm hover:bg-zinc-200 dark:hover:bg-zinc-700">
-                                {theme === 'dark' ? 'Terang' : 'Gelap'}
-                                <FontAwesomeIcon icon={theme === 'dark' ? faSun : faMoon} className="w-3 h-3 text-inherit" />
-                            </button>
-                        </div>
+                    )}
+                </div>
+                <div className="px-5 md:px-10 pt-5 pb-10 relative overflow-auto w-full h-screen">
+                    <div className="p-5 border dark:border-zinc-800 bg-white dark:bg-zinc-900 rounded-2xl">
+                        {children}
                     </div>
                 </div>
-                <div className={`${rale.className} w-full md:w-10/12 px-5 pt-16 h-full text-zinc-800 relative overflow-auto`}>
-                    <hr className="block md:hidden my-1 opacity-0" />
-                    <h1 className={`${rale.className} text-blue-600 md:hidden flex font-medium text-xl tracking-wide w-fit  items-center gap-3`}>
-                        <FontAwesomeIcon icon={ filteredPath ? filteredPath.icon : faSpinner} className={`w-4 h-4 text-inherit ${!filteredPath ? 'animate-spin' : ''}`} />
-                        { filteredPath ? filteredPath.page : '' }
-                    </h1>
-                    <hr className="my-1 opacity-0" />
-                    {children}
-                </div>
+            </div> 
+            <div className="drawer-side">
+                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                {/* Sidebar content here */}
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+                
+                </ul>
             </div>
         </div>
     )
