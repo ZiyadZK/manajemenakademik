abcbbeb97d01d5d2c14a56e4720f1c6bda866eef
Ziyad <kakangtea74@gmail.com>
2024-07-18 14:34:50 +0700

    fixing update

--- src/app/data/siswa/page.jsx
+++ src/app/data/siswa/page.jsx
@@ -1774,7 +1774,7 @@ export default function DataSiswaPage() {
                                                         Jenis Kelamin
                                                     </p>
                                                     <div className="w-full md:w-2/3">
-                                                        <select required defaultValue={value['jurusan']} className="px-3 py-2 rounded-md w-full dark:bg-zinc-900 border dark:border-zinc-800">
+                                                        <select required defaultValue={value['jenis_kelamin']} className="px-3 py-2 rounded-md w-full dark:bg-zinc-900 border dark:border-zinc-800">
                                                             <option value="" disabled>-- Pilih Jenis Kelamin --</option>
                                                             <option value="Laki-laki">Laki-laki</option>
                                                             <option value="Perempuan">Perempuan</option>
